b82a945551304871ececcf2324dceeb6df42ed82
jwonged <dsjw2@cam.ac.uk>
2017-02-17 03:50:30 +0000

    Update csvFunc.py

--- csvFunc.py
+++ csvFunc.py
@@ -2,5 +2,5 @@ import csv
 
 outputFile = open('output.csv', 'w', newline='')
 outputWriter = csv.writer(outputFile)
-outputWriter.writerow(['row1','row2','row3','ham'])
+outputWriter.writerow(['row1','row2','row3'])
 outputFile.close()
